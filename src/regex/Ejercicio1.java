4e9a7550024671253b4e97fe1c9c6e87ea5c1d23
Julio Pedro López Bayón <jplb.fp@gmail.com>
2023-10-31 16:47:54 +0100

    Actualización

---
+++ src/regex/Ejercicio1.java
@@ -0,0 +1,41 @@
+package regex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ *	Escribe un programa que use una expresión regular para
+ *  encontrar nombres de personas en un texto, teniendo en
+ *  cuenta que un nombre es válido si cumple las condiciones
+ *  siguientes:
+ *  
+ *  	* Está formado por dos o más palabras.
+ *  	* Cada palabra comienza por una letra mayúscula seguida
+ *        de letras minúsculas.
+ *      * Cada palabra debería tener al menos dos letras.
+ *      * Las palabras se separan entre sí por un único espacio
+ *        en blanco.
+ *        
+ *  El usuario escribirá en la consola una línea que contendrá
+ *  varios nombres separados por comas y el programa la leerá,
+ *  buscará los nombres y los mostrará de nuevo, cada uno en una línea. 
+ * 
+ */
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		Pattern p = Pattern.compile("[A-ZÁÉÍÓÚ]\\p{L}+(?:\\s[A-ZÁÉÍÓÚ]\\p{L}+)+");
+		System.out.print("> ");
+		String nombres = in.readLine();
+		Matcher m = p.matcher(nombres);
+		while (m.find()) {
+			System.out.println(m.group());
+		}
+	}
+
+}
---
+++ src/regex/Ejercicio2.java
@@ -0,0 +1,30 @@
+package regex;
+
+/*
+ * Escribe un programa que use una expresión regular para validar
+ * fechas, teniendo en cuenta que una fecha es válida si cumple
+ * las condiciones siguientes:
+ * 
+ * 		* Comienza por dos dígitos para el día del mes seguidos
+ *        de un separador.
+ *      * A continuación, le siguen tres letras para el mes o dos
+ *        dígitos para el número del mes, seguidos de un separador.
+ *      * A continuación, le siguen cuatro dígitos para el año. 
+ *      * El separador puede ser un guion ('-') o la barra inclinada
+ *        ('/').
+ *      * No se pueden combinar diferentes separadores en una misma
+ *        fecha.
+ *        
+ * El usuario introducirá fechas y el programa tendrá que determinar
+ * si son válidas o no.
+ *  
+ * 
+ */
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
