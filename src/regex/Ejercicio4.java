b7b8d89953cfbf5d2dcba32fe8afbae3be09a42b
Julio Pedro López Bayón <jplb.fp@gmail.com>
2023-11-10 18:27:18 +0100

    Actualización

--- src/regex/Ejercicio4.java
+++ src/regex/Ejercicio4.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /*
- *  Escribe un programa que extraiga todas las URLs que comiencen por
- *  http:// o https:// contenidas en un documento HTML almacenado en un
- *  fichero. La ruta de acceso al fichero se especificará en un parámetro
- *  de línea de comando.
+ *  Escribe un programa que utilice un Scanner para extraer y mostrar todos los hiperenlaces
+ *  de un documento HTML almacenado en un fichero cuya ruta de acceso se especificará en un
+ *  parámetro de línea de comando, teniendo en cuenta que cada hiperenlace está asignado al
+ *  parámetro href de una etiqueta a:
  *  
+ *  			<a … href="hiperenlace" …>
+ *    
  */
 
 public class Ejercicio4 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println(args[0]);
-		Pattern regex = Pattern.compile("");
+		Pattern regex = Pattern.compile("<a.*?href=\"(.*?)\"");
 		try (Scanner s = new Scanner(new File(args[0]))) {
-			String url;
-			while ((url = s.findWithinHorizon(regex, 0)) != null) {
-				System.out.println(url);
+			while (s.findWithinHorizon(regex, 0) != null) {
+				System.out.println(s.match().group(1));
 			}
 		}
 	}
