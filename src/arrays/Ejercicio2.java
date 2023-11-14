d8250ef3680dccef0565a167208cb6198e919deb
Julio Pedro López Bayón <jplb.fp@gmail.com>
2023-11-14 16:55:06 +0100

    Actualización

--- src/arrays/Ejercicio2.java
+++ src/arrays/Ejercicio2.java
@@ -21,25 +21,64 @@ import java.util.Random;
 
 public class Ejercicio2 {
 
-	public static void main(String[] args) {
-		Random r = new Random();
-		int [] v = new int[r.nextInt(41) + 10];
-		for (int i = 0; i < v.length; i++) {
-			int n;
-			do {
-				n = r.nextInt(201) - 100;
-			} while (contiene(v, n)); /* mientras el array ya contenga a n */
-			v[i] = n;
-		}
-		
-		System.out.println(Arrays.toString(v));
-	}
+//	public static void main(String[] args) {
+//		Random r = new Random();
+//		int [] v = new int[r.nextInt(41) + 10];
+//		for (int i = 0; i < v.length; i++) {
+//			int n;
+//			do {
+//				n = r.nextInt(201) - 100;
+//			} while (contiene(v, n, i)); /* mientras el array ya contenga a n */
+//			v[i] = n;
+//		}
+//		System.out.println(Arrays.toString(v));
+//		System.out.println(Arrays.toString(crearInverso(v)));
+//	}
 
 	/*
 	 * retorna true si v contiene a n
 	 */
-	static boolean contiene(int[] v, int n) {
-		
+//	static boolean contiene(int[] v, int n) {
+//		int i=0;
+//		boolean encontrado = false;
+//		while (i < v.length && !encontrado)
+//			if (v[i++] == n)
+//				encontrado = true;
+//		return encontrado;
+//	}
+	
+//	static boolean contiene(int[] v, int n, int iMax) {
+//		int i=0;
+//		while (i < iMax && v[i++] != n);
+//		return i < iMax;
+////		if (i < imax)
+////			return true;
+////		else
+////			return false;
+//	}
+	
+	static int [] crearInverso(int [] v) {
+		int [] vInverso = new int[v.length];
+		for (int i = v.length - 1; i >= 0; i--)
+			vInverso[v.length - 1 - i] = v[i];
+		return vInverso;
 	}
+	
+	public static void main(String[] args) {
+		Random r = new Random();
+		int [] v = new int[r.nextInt(41) + 10];
+		int i=0;
+		while (i < v.length) {
+			int n = r.nextInt(201) - 100;
+			int j = 0;
+			while (j < i && v[j++] != n);
+			if (j == i)
+				v[i] = n;
+			i++;
+		}
+		System.out.println(Arrays.toString(v));
+		System.out.println(Arrays.toString(crearInverso(v)));
+	}
+	
 	
 }
