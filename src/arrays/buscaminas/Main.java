ee50c984f127574f3f536eaa98d921270582348d
Julio Pedro López Bayón <jplb.fp@gmail.com>
2023-11-24 19:30:41 +0100

    Actualización

---
+++ src/arrays/buscaminas/Main.java
@@ -0,0 +1,24 @@
+package arrays.buscaminas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+	/*
+	 * filas: número de filas del tablero
+	 * columnas: número de columnas del tablero
+	 * minas: número de minas
+	 * tablero[fil][col]: casilla en la que no se puede colocar una mina
+	 * 
+	 * Crea el tablero y coloca el número de minas especificado de forma aleatoria
+	 */
+	static int [][] crearTablero(int filas, int columnas, int minas, int fil, int col) {
+		int [][] tablero = new int[filas][columnas];
+		
+		
+		return tablero;
+	}
+	
+}
