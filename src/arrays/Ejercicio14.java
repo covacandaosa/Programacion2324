9c47e58629d917446d5a59590032481771c076ab
Julio Pedro López Bayón <jplb.fp@gmail.com>
2023-11-28 16:28:56 +0100

    Actualización

--- src/arrays/Ejercicio14.java
+++ src/arrays/Ejercicio14.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Ejercicio14 {
 
 	public static void main(String[] args) {
-		int [] v1 = Ejercicio05.rellenarSinRepetir(10, 20, 1, 999);
-		int [] v2 = Ejercicio05.rellenarSinRepetir(10, 20, 1, 999);
+		int [] v1 = Ejercicio05.rellenarSinRepetir(3, 5, 1, 999);
+		int [] v2 = Ejercicio05.rellenarSinRepetir(3, 5, 1, 999);
 		int [] v3 = new int[v1.length + v2.length];
 		Arrays.sort(v1);
 		Arrays.sort(v2);
@@ -14,15 +14,25 @@ public class Ejercicio14 {
 		int i1 = 0;
 		int i2 = 0;
 		int i3 = 0;
-		while () {
-			
+		while (i1 < v1.length && i2 < v2.length) {
+			if (v1[i1] > v2[i2]) {
+				v3[i3] = v2[i2];
+				i2++;
+			}
+			else {
+				v3[i3] = v1[i1];
+				i1++;
+			}
+			i3++;
 		}
-		if () {
-			
+		if (i1 < v1.length) {
+			for (; i1<v1.length; i1++, i3++)
+				v3[i3] = v1[i1]; 
 		}		
-		else if () {
-			
-		}	
+		else if (i2 < v2.length) {
+			for (; i2<v2.length; i2++, i3++)
+				v3[i3] = v2[i2]; 
+		}		
 		
 		System.out.println(Arrays.toString(v1));
 		System.out.println(Arrays.toString(v2));
