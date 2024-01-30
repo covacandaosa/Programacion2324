de0b7deb3405fb62e8720713c5aaf324e3dfef75
Julio Pedro López Bayón <jplb.fp@gmail.com>
2024-01-30 16:55:01 +0100

    Actualización

--- src/colecciones/i/Ejercicio5.java
+++ src/colecciones/i/Ejercicio5.java
@@ -16,11 +16,18 @@ public class Ejercicio5 {
 			System.out.print("> ");
 			for (int i=0; i<n; i++)
 				cola.offer(in.nextInt());
-			for (int i=0; i<k; i--)
+			for (int i=0; i<k; i++)
 				cola.poll();
-			
-				
+			boolean xEstaAlmacenado = false;
+			int min = Integer.MAX_VALUE;
+			for (int i=0; i<cola.size(); i++) {
+				int v = cola.poll();
+				if (v < min)
+					min = v;
+				xEstaAlmacenado = (v == x);
+				cola.offer(v);
 			}
+			System.out.println(xEstaAlmacenado ? true : min);
 		}
 	}
 
--- src/colecciones/i/ejercicio4/Palabras.java
+++ src/colecciones/i/ejercicio4/Palabras.java
@@ -3,6 +3,7 @@ package colecciones.i.ejercicio4;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,9 +17,48 @@ public class Palabras {
 	
 	public Palabras(String texto) {
 		this();
+		añadirPalabras(texto);
+	}
+	
+	public void añadirPalabras(String texto) {
 		Matcher m = Pattern.compile("\\S+").matcher(texto);
-		while (m.find()) {
-			
-		}
+		while (m.find())
+			añadirPalabra(m.group());
+	}
+	
+//	public boolean añadirPalabra(String palabra) {
+//		int longitud = palabra.length();
+//		Set<String> set;
+//		if (palabras.containsKey(longitud)) {
+//			set = palabras.get(longitud);
+//			return set.add(palabra);
+//		}
+//		else {
+//			set = new TreeSet<>();
+//			boolean añadida = set.add(palabra);
+//			palabras.put(longitud, set);
+//			return añadida;
+//		}
+//	}
+	
+	
+//	public boolean añadirPalabra(String palabra) {
+//		int longitud = palabra.length();
+//		Set<String> set = palabras.get(longitud);;
+//		if (set == null)
+//			palabras.put(longitud, set = new TreeSet<>());
+//		return set.add(palabra);
+//	}
+	
+	public boolean añadirPalabra(String palabra) {
+		int longitud = palabra.length();
+		palabras.computeIfAbsent(longitud, l -> new TreeSet<>());
+		return palabras.get(longitud).add(palabra);
+	}
+	
+	public boolean contienePalabra(String palabra) {
+		Set<String> set = palabras.get(palabra.length());
+//		return set == null ? false : set.contains(palabra);
+		return set != null && set.contains(palabra);
 	}
 }
